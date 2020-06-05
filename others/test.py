e8b8446455fe913b0737faa22bcf86784451d493
Incipe-win <whc_9_13@163.com>
2020-06-05 13:15:42 +0800

    update

--- others/test.py
+++ others/test.py
@@ -1,14 +1,21 @@
-class Cat:
+def func(a, b, *argc, **argv):
+    print(a)
+    print(type(a))
+    print(b)
+    print(type(b))
+    print(argc)
+    print(type(argc))
+    print(argv)
+    print(type(argv))
+    print("*" * 20)
+    print(*argc)
+    return argv
 
-    def __init__(self, new_name):
-        print("相当于构造函数！(带参数)")
-        self.name = new_name
 
-    def eat(self):
-        print("%s 爱吃！" % self.name)
-
-
-tom_cat = Cat("Tom")
-lazy_cat = Cat("LazyCat")
-tom_cat.eat()
-lazy_cat.eat()
+# value1, value2 = func(1, 2, 3, 4, {1: 1, "2": "2"})
+# print(type(func(1, 2, 3, 4, {1: 1, "2": "2"})))
+# print(value1)
+# print(value2)
+ret = func(1, 2, 3, 4, {1: 1, "2": "2"}, x=1, y=2)
+# ret = func(1, 2, 3, 4, {1: 1, "2": "2"})
+print(ret)
